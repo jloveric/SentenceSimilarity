5129c4659a2edf847ae0c0c342f25df85a117115
jloveric <john.loverich@gmail.com>
2017-04-29 16:30:06 -0600

    Adding and input point for the require.

---
+++ index.js
@@ -0,0 +1,6 @@
+
+let similarityScore = require('./SimilarityScore.js')
+let sentenceSimilarity = require('./SentenceSimilarity.js')
+
+module.exports.sentenceSimilarity = sentenceSimilarity;
+module.exports.similarityScore = similarityScore;
