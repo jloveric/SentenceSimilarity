9bde8dac82b509456e4f3fbed6ec1db0eb0736a8
John Loverich <john.loverich@gmail.com>
2019-10-28 18:23:40 -0700

    Formatting all js files.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-"use strict"
+"use strict";
 
-let similarityScore = require('./SimilarityScore.js')
-let sentenceSimilarity = require('./SentenceSimilarity.js')
+let similarityScore = require("./SimilarityScore.js");
+let sentenceSimilarity = require("./SentenceSimilarity.js");
 
 module.exports.sentenceSimilarity = sentenceSimilarity;
 module.exports.similarityScore = similarityScore;
