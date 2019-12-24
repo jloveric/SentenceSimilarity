2e12becd6c45f614345ed22084c5e6a3d2e0be96
John Loverich <john.loverich@gmail.com>
2019-12-23 19:33:29 -0800

    Simplifying require.

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 "use strict";
 
-let similarityScore = require("./SimilarityScore.js");
 let sentenceSimilarity = require("./SentenceSimilarity.js");
 
-module.exports.sentenceSimilarity = sentenceSimilarity;
-module.exports.similarityScore = similarityScore;
+module.exports = sentenceSimilarity;
